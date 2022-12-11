1e4182fb8f8dd74bf50ec8d292890323cb9eabff
BenCottrell <bencottrell48@yahoo.com>
2022-12-11 12:57:12 -0500

    removed console log for install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -23,6 +23,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
     window.deferredPrompt = null;
 });
