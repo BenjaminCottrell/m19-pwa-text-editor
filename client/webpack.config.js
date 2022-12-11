2d781166aa58df4dd646548b2cd9b7a48b28292f
BenCottrell <bencottrell48@yahoo.com>
2022-12-10 23:42:11 -0500

    added properties to webpackpwamanifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      new WebpackPwaManifest(),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        fingerprints: false,
+        inject: true,
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
